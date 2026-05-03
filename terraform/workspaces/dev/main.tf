5bfc5bfc02895a21357e56b8597d67900f5b4fd9
edwilson <edward.e.j.wilson@gmail.com>
2026-05-03 13:28:04 +0100

    Add version constraints for terraform providers
    - To fix `tflint` errors.

--- terraform/workspaces/dev/main.tf
+++ terraform/workspaces/dev/main.tf
@@ -1,5 +1,6 @@
 module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "6.4.0"
 
   name              = "pilates-gpt"
   instance_type     = "t3.micro"
