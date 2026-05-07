cd5b0900ba43c1d150edbae80fdc17ac5f780da4
edwilson <edward.e.j.wilson@gmail.com>
2026-05-07 09:45:09 +0100

    Delete duplicate `unit_of_work` fixture from functional tests

--- backend/tests/functional/api/conftest.py
+++ backend/tests/functional/api/conftest.py
@@ -1,14 +1,10 @@
-import pathlib
 import typing
 
-import pytest
 import pytest_asyncio
 from fastapi import testclient
 
 from pilates import config
-from pilates.data import json_backend
 from pilates.interfaces.api import app
-from testing.helpers import unit_of_work as unit_of_work_helpers
 
 
 class _APIClient(testclient.TestClient):
@@ -25,18 +21,3 @@ class _APIClient(testclient.TestClient):
 async def api_client(unit_of_work) -> typing.AsyncIterator[_APIClient]:
     async with app.lifespan(app.app):
         yield _APIClient(app.app)
-
-
-@pytest.fixture()
-def unit_of_work(
-    tmp_path: pathlib.Path,
-) -> typing.Generator[json_backend.JSONUnitOfWork, None, None]:
-    """
-    Use the real repository implementation, but with a fresh database per test.
-    """
-    # Create a fresh JSON database in a temp directory.
-    database_file = tmp_path / "test_database.json"
-    uow = json_backend.JSONUnitOfWork(database_file=database_file)
-
-    with unit_of_work_helpers.inject_uow(uow):
-        yield uow
