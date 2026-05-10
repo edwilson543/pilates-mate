4b1b61bd77040685ac6029087515d0763c7faac0
edwilson <edward.e.j.wilson@gmail.com>
2026-05-10 06:46:04 +0100

    Mark `/lesson-plans` the landing page post-login

--- frontend/src/app/(authenticated)/page.tsx
+++ frontend/src/app/(authenticated)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/exercises");
+    router.push("/lesson-plans");
   }, [router]);
 
   return null;
